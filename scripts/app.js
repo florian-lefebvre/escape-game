f07cf96386eb8ed5f0a5b5c76b2316ce23ad3bc9
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-12-01 21:44:43 +0100

    finish vuejs migration

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,38 @@
-class Game {
-  inventory = [];
+import { createApp } from "vue";
 
-  async init() {}
-}
+const params = new URLSearchParams(window.location.search);
 
-const game = new Game();
+createApp({
+  data() {
+    return {
+      data: null,
+      inventory: ["test"],
+      progress: 0,
+      urlParams: {
+        id: params.get("id"),
+      },
+    };
+  },
+  mounted() {
+    this.loadStoredData();
+    this.fetchData();
+  },
+  methods: {
+    loadStoredData() {
+      const parsed = JSON.parse(localStorage.getItem("inventory"));
+      if (parsed) {
+        this.inventory = parsed;
+      }
+    },
+    async fetchData() {
+      const res = await fetch("./data.json");
+      this.data = await res.json();
+    },
+  },
+  watch: {
+    inventory(n) {
+      console.log(n);
+      localStorage.setItem("inventory", JSON.stringify(n));
+    },
+  },
+}).mount("#app");
